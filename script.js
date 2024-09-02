d3ecec0c2be1abf7de64042d26a0f752636af436
Junus Ergin <JunusErgin@users.noreply.github.com>
2024-09-02 11:44:14 +0200

    Character mit Tasten animieren

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let x = 0;
 
-setInterval(moveCharacter, 75);
+// setInterval(moveCharacter, 75);
+
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+       moveCharacter();
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       moveCharacter();
+    }
+}
 
 function moveCharacter(){
     orc.style = `object-position: -${x * 200}px;`;
