ed71c5981220c4af61260041c928007636abf54c
Junus Ergin <JunusErgin@users.noreply.github.com>
2024-09-02 15:00:27 +0200

    Character smooth bewegen

--- script.js
+++ script.js
@@ -1,41 +1,78 @@
 let x = 0;
 let left = 100;
 let bottom = 250;
-// setInterval(moveCharacter, 75);
-
-
+let leftArrow = false;
+let rightArrow = false;
+let topArrow = false;
+let bottomArrow = false;
 
 document.onkeydown = checkKey;
+document.onkeyup = unCheckKey;
 
 function checkKey(e) {
-
     e = e || window.event;
 
     if (e.keyCode == '38') {
-        // up arrow
-        bottom += 5;
-        moveCharacter();
-        orc.style.bottom = `${bottom}px`;
+        topArrow = true;
     }
     else if (e.keyCode == '40') {
-        // down arrow
-        bottom -= 5;
-        moveCharacter();
-        orc.style.bottom = `${bottom}px`;
+        bottomArrow = true;
     }
     else if (e.keyCode == '37') {
-        // left arrow
-        left -= 5;
-        moveCharacter();
-        orc.style.left = `${left}px`;
+        leftArrow = true;
+
     }
     else if (e.keyCode == '39') {
-        // right arrow
-        left += 5;
-        moveCharacter();
-        orc.style.left = `${left}px`;
+        rightArrow = true;
+    }
+
+
+}
+
+function unCheckKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        topArrow = false;
+    }
+    else if (e.keyCode == '40') {
+        bottomArrow = false;
+    }
+    else if (e.keyCode == '37') {
+        leftArrow = false;
+
+    }
+    else if (e.keyCode == '39') {
+        rightArrow = false;
     }
 }
+
+setInterval(updateGame, 25);
+
+function updateGame() {
+    if (topArrow) {
+        bottom += 5;
+    }
+
+    if (bottomArrow) {
+        bottom -= 5;
+    }
+
+    if (leftArrow) {
+        left -= 5;
+    }
+
+    if (rightArrow) {
+        left += 5;
+    }
+
+    if (topArrow || bottomArrow || leftArrow || rightArrow) {
+        moveCharacter();
+    }
+    orc.style.left = `${left}px`;
+    orc.style.bottom = `${bottom}px`;
+}
+
 
 function moveCharacter() {
     orc.style.objectPosition = `-${x * 200}px`;
